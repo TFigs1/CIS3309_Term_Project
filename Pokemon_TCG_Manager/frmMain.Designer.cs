03566d929b6560c3385f4a14dbefd3d56649a63a
TFigs1 <149716684+TFigs1@users.noreply.github.com>
2025-12-07 13:52:58 -0500

    Tested wishlist - seems to work fine although move to collections button seems to be messed up - maybe just remove? removed temp designer buttons from frmMain - moved back to fully runtime generated controls as per the reqs

--- Pokemon_TCG_Manager/frmMain.Designer.cs
+++ Pokemon_TCG_Manager/frmMain.Designer.cs
@@ -28,64 +28,22 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.btnGoToCollections = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
-            this.btnWishlists = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // btnGoToCollections
-            // 
-            this.btnGoToCollections.Location = new System.Drawing.Point(284, 215);
-            this.btnGoToCollections.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.btnGoToCollections.Name = "btnGoToCollections";
-            this.btnGoToCollections.Size = new System.Drawing.Size(140, 19);
-            this.btnGoToCollections.TabIndex = 0;
-            this.btnGoToCollections.Text = "Go to collections";
-            this.btnGoToCollections.UseVisualStyleBackColor = true;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(294, 200);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(123, 13);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Card Collection Manager";
-            // 
-            // btnWishlists
-            // 
-            this.btnWishlists.Location = new System.Drawing.Point(284, 238);
-            this.btnWishlists.Margin = new System.Windows.Forms.Padding(2);
-            this.btnWishlists.Name = "btnWishlists";
-            this.btnWishlists.Size = new System.Drawing.Size(140, 19);
-            this.btnWishlists.TabIndex = 2;
-            this.btnWishlists.Text = "Wishlist";
-            this.btnWishlists.UseVisualStyleBackColor = true;
-            this.btnWishlists.Click += new System.EventHandler(this.btnWishlists_Click);
             // 
             // frmMain
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(740, 496);
-            this.Controls.Add(this.btnWishlists);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.btnGoToCollections);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.ClientSize = new System.Drawing.Size(1480, 954);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "frmMain";
             this.Text = "Pokemon TCG Manager ";
             this.Load += new System.EventHandler(this.frmMain_Load);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Button btnGoToCollections;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button btnWishlists;
     }
 }
 
